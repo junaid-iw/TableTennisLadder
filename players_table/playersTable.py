c23901d06510d6b04c14001c415300f1c494654f
matt-twomey-iw <matt.twomey@infinityworks.com>
2018-09-17 10:22:33 +0100

    Directories added

---
+++ players_table/playersTable.py
@@ -0,0 +1,21 @@
+class PlayersTable:
+    def __init__(self, players):
+        self.players = players
+    
+    def getPlayers(self):
+        return self.players
+    
+    def setPlayers(self, newplayers):
+        self.players = newplayers
+
+    def playerInTable(self, player):
+        if player in self.players:
+            return True
+        else:
+            return False
+            
+    def addPlayer(self, player):
+        self.players.append(player)
+
+    def removePlayer(self, player):
+        self.players.remove(player)
