8958514aea6bedf357d80ed60192ae1f060ee291
matt-twomey-iw <matt.twomey@infinityworks.com>
2018-09-18 13:33:28 +0100

    Ready for Spoons and Ash

--- io/file_handler.py
+++ io/file_handler.py
@@ -1,15 +1,44 @@
 from player.player import Player
 from leaderboard.leaderboard import Leaderboard
 from players_table.playersTable import PlayersTable
+from file_read_write import FileReadWrite
+
+# fileReadWrite = FileReadWrite("player/storedPlayers.txt")
+# fileHandler = FileHandler(fileReadWrite)
+# playersList = FileHandler.readPlayers
 
 class FileHandler:
     
+    def __init__(self, fileReadWrite):
+        self.fileReadWrite = fileReadWrite
+
 #
 # READ FILE METHODS
 #
 
+    def getFileList(self):
+        fileContents = self.fileReadWrite.readFile()
+        fileList = fileContents.splitlines()
+        return fileList
+
+    def getPlayersFromFile(self):
+        playerNames = self.fileReadWrite.readFile()
+        players = []
+        for name in playerNames:
+            player = Player(name)
+            players.append(player)
+        return players
+
+    def getPlayersTableFromFile(self):
+        players = getPlayersFromFile()
+        playersTable = PlayersTable(players)
+    
+    def getLeaderboardFromFile(self, leaderboardName):
+        players = getPlayersFromFile()
+        Leaderboard = Leaderboard(leaderboardName, players)
+
     def readPlayers(self):
-        playerNames = self.readFile("player/storedPlayers.txt")
+        playerNames = self.fileReadWrite.readFile()
         players = []
         for name in playerNames:
             player = Player(name)
@@ -30,8 +59,39 @@ class FileHandler:
         leaderboardNames = self.readFile("leaderboard/leaderboardNames.txt")
         return leaderboardNames
 
-    def readFile(self, filename):
-        myFile = open(filename, "r")
-        contents = myFile.read().splitlines()
-        myFile.close()
-        return contents
+    #
+    # WRITE FILE METHODS
+    #
+
+    def updateFile(self, listForFile):
+        contents = ""
+        for item in listForFile:
+            contents.append(item + "\n")
+        self.fileReadWrite.writeFile(contents)
+
+    def updatePlayersTable(self, playersTable):
+        players = playersTable.getPlayers()
+        playerNames = []
+        for player in players:
+            playerNames.append(player.getName())
+        updateFile("player/storedPlayers.txt", playerNames)
+
+    def updateLeaderboard(self, leaderboard):
+        players = leaderboard.getRankings()
+        playerNames = []
+        for player in players:
+            playerNames.append(player.getName())
+        leaderboardName = leaderboard.getName()
+        updateFile("leaderboard/" + leaderboardName + ".txt", playerNames)
+
+    def updateLeaderboardNames(self, leaderboardNames):
+        updateFile("leaderboard/leaderboardNames.txt", leaderboardNames)
+
+    # Reorders the leaderboardNames.txt file so the current leaderboard's name is first
+    def reorderLeaderboardNames(self, newCurrentLeaderboardName, leaderboardNames):
+        if newCurrentLeaderboardName in leaderboardNames:
+            newCurrentLearderboardNamePosition = leaderboardNames.index(newCurrentLeaderboardName)
+            reorderedLeaderboardNames = [newCurrentLeaderboardName] + leaderboardNames[:newCurrentLearderboardNamePosition] + leaderboardNames[newCurrentLearderboardNamePosition + 1:]
+        else:
+            reorderedLeaderboardNames = [newCurrentLeaderboardName] + leaderboardNames
+        updateFile("leaderboard/leaderboardNames.txt", reorderedLeaderboardNames)
